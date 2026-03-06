d634f77ba4fcdf0b0bde36cbe64ce1adede771de
gaoteng <teng@0g.ai>
2026-03-06 10:18:53 +0100

    revert useless update

--- src/core/oxm/mongo/document_base.py
+++ src/core/oxm/mongo/document_base.py
@@ -8,8 +8,7 @@ from datetime import datetime
 from common_utils.datetime_utils import to_timezone
 from beanie import Document, WriteRules
 from pydantic import model_validator, BaseModel
-from typing import List, Optional
-from typing_extensions import Self
+from typing import Self, List, Optional
 from pymongo.asynchronous.client_session import AsyncClientSession
 from pymongo.results import InsertManyResult
 
